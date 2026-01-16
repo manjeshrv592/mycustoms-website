215af3a2448a39695cfe60e7ee6568bb0ae83485
manjesh <manjeshrv592@gmail.com>
2026-01-16 12:37:13 +0530

    Adjusted mt for green title in resources

--- src/app/[lang]/resources/blogs/[slug]/page.tsx
+++ src/app/[lang]/resources/blogs/[slug]/page.tsx
@@ -100,7 +100,7 @@ export default async function BlogPage({ params }: BlogPageProps) {
         <ResourcesSecondaryNav currentPage="blogs" lang={currentLang} />
       </div>
 
-      <div className="flex md:w-[65%] gap-2 flex-col">
+      <div className="flex md:w-[65%] gap-2 flex-col bg-red-500 mt-2">
         <div className="flex items-center gap-4">
           <span className="inline-block h-px w-[50px] bg-[#7ED957]">
             &nbsp;
