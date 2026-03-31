c2d2adb4d034dcc13ae990ae6ee36469c9136e1c
manjeshrv592 <manjeshrv592@gmail.com>
2026-03-31 14:28:08 +0530

    Made contact image smaller

--- src/components/contact/ContactInfoSection.tsx
+++ src/components/contact/ContactInfoSection.tsx
@@ -31,14 +31,14 @@ const ContactInfoSection = ({
 
   return (
     <div
-      className={`lg:w-[75%] absolute md:relative md:flex flex-col gap-4 w-full min-w-0 2xl:max-w-[36vw] ml-auto bottom-0 left-0 md:left-auto p-4 md:p-0 transition-all duration-500 ease-in-out overflow-hidden 2xl:max-h-none ${
+      className={`lg:w-[60%] absolute md:relative md:flex flex-col gap-4 w-full min-w-0 2xl:max-w-[36vw] ml-auto bottom-0 left-0 md:left-auto p-4 md:p-0 transition-all duration-500 ease-in-out overflow-hidden 2xl:max-h-none ${
         isInfoCollapsed
           ? "max-h-0 opacity-0 md:max-h-none md:opacity-100"
           : "max-h-[500px] opacity-100 2xl:max-h-none"
       }`}
     >
-      <div className="lg:flex-1 md:flex lg:items-end w-full min-w-0 ">
-        <div className="w-full rounded-xl overflow-hidden relative border border-[#dcdcdc] p-4 flex items-end h-[40vh] md:h-[30vh] xl:h-[40vh]">
+      <div className="lg:flex-1 md:flex lg:items-end w-full min-w-0">
+        <div className="w-full rounded-xl overflow-hidden relative border border-[#dcdcdc] p-1 flex items-end h-[40vh] md:h-[30vh] xl:h-[30vh]">
           {/* Contact Section - Image */}
           <Image
             src={contactImageUrl}
@@ -48,7 +48,7 @@ const ContactInfoSection = ({
             priority
             className="object-cover absolute"
           />
-          <div className="z-20 text-white relative border border-white py-1 px-4 rounded-lg bg-black/10 backdrop-blur-[5px] flex-1 text-xs flex flex-col gap-2">
+          <div className="z-20 text-white relative border border-white py-1 px-4 rounded-lg bg-black/10 backdrop-blur-[5px] flex-1 text-xs flex flex-col lg:flex-row lg:justify-between lg:items-center gap-2">
             <div>
               <div className="">{basedAtTitle}</div>
               {/* Address */}
@@ -56,7 +56,7 @@ const ContactInfoSection = ({
             </div>
             <div className="">
               <a
-                className="text-[#3871c1] font-bold border-b border-[#3871c1]"
+                className="text-white font-bold border-b border-white"
                 target="_blank"
                 href={mapLink}
               >
@@ -66,7 +66,7 @@ const ContactInfoSection = ({
           </div>
         </div>
       </div>
-      <div className="flex justify-between pt-4 border-t border-white mt-4 lg:mt-0 flex-col lg:flex-row">
+      <div className="flex justify-between pt-4 border-t border-white mt-4 lg:mt-0 flex-col lg:flex-row text-xs">
         <a href={`tel:${phone}`} className="flex gap-2">
           <span className="rounded-full bg-transparent size-8 border border-white flex items-center justify-center">
             <Phone className="size-3" />
